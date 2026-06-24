804d020a8b84eed0747f2f1f46dd10a0383f6c70
Atharv <172103387+zokomom@users.noreply.github.com>
2026-06-24 21:34:03 +0530

    feat: add download_from_s3 function and update resize_image to use S3 for input

--- app/services/image_service.py
+++ app/services/image_service.py
@@ -1,19 +1,32 @@
 from PIL import Image
 import ffmpeg
-from ..services.s3_client import upload_to_s3
+from ..services.s3_client import upload_to_s3, download_from_s3
+
 
 def resize_image(filename):
 
-    img = Image.open(f"app/uploads/{filename}")
+    local_input = f"/tmp/{filename}"
 
-    img = img.resize((500,500))
+    download_from_s3(
+        f"uploads/{filename}",
+        local_input
+    )
+
+    img = Image.open(local_input)
+
+    img = img.resize((500, 500))
 
     img.save(f"app/processed/{filename}")
 
+    local_output = f"/tmp/processed_{filename}"
+
+    img.save(local_output)
+
     upload_to_s3(
-        f"app/processed/{filename}",
+        local_output,
         f"processed/{filename}"
     )
+
 
 def compress_video(filename):
     ffmpeg.input(f"app/uploads/{filename}").output(
--- app/services/s3_client.py
+++ app/services/s3_client.py
@@ -2,11 +2,20 @@ import boto3
 
 s3 = boto3.client("s3")
 
-BUCKET_NAME ="atharv-media-storage"
+BUCKET_NAME = "atharv-media-storage"
+
 
 def upload_to_s3(local_path, s3_key):
     s3.upload_file(
         local_path,
         BUCKET_NAME,
         s3_key
+    )
+
+
+def download_from_s3(s3_key, local_path):
+    s3.download_file(
+        BUCKET_NAME,
+        s3_key,
+        local_path
     )
