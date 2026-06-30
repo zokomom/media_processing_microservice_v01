21ced65ef88330126f91046cafa1b1ae55da970f
Atharv <172103387+zokomom@users.noreply.github.com>
2026-07-01 00:44:06 +0530

    feat: enhance process_image and process_video functions to ensure job status updates during processing(related to job status)

--- app/tasks.py
+++ app/tasks.py
@@ -4,18 +4,33 @@ from app.services.image_service import (
     compress_video,
     generate_thumbnail
 )
+from .services.job_services import update_job_status
 
 
 @celery_app.task
 def process_image(filename, job_id):
-    print(f"Processing image: {filename}")
-    resize_image(filename)
-    print(f"Completed image: {filename}")
+    update_job_status(job_id, "processing")
+    try:
+        print(f"Processing image: {filename}")
+        resize_image(filename)
+        update_job_status(job_id, "completed")
+        print(f"Completed image: {filename}")
+    except Exception as e:
+        update_job_status(job_id, "failed")
+        print(f"Error while processing {filename}: {e}")
+        raise
 
 
 @celery_app.task
 def process_video(filename, job_id):
-    print(f"Processing video: {filename}")
-    compress_video(filename)
-    generate_thumbnail(filename)
-    print(f"Completed video: {filename}")
+    update_job_status(job_id, "processing")
+    try:
+        print(f"Processing video: {filename}")
+        compress_video(filename)
+        generate_thumbnail(filename)
+        update_job_status(job_id, "completed")
+        print(f"Completed video: {filename}")
+    except Exception as e:
+        update_job_status(job_id, "failed")
+        print(f"Error while processing {filename}: {e}")
+        raise
