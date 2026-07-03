a5a5242a18b705a60c60f49df471a422e03e16ce
Atharv <172103387+zokomom@users.noreply.github.com>
2026-07-03 17:41:43 +0530

    refactor: refactor file upload handling and media processing to use temporary files

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -4,12 +4,25 @@ from ..services.s3_client import upload_to_s3
 import uuid
 from app.tasks import process_image, process_video
 from ..services.job_services import create_job
+import os
 
 router = APIRouter()
+
+ALLOWED_TYPES = {
+    "image/jpeg",
+    "image/png",
+    "video/mp4"
+}
 
 
 @router.post("/upload", status_code=status.HTTP_201_CREATED)
 async def upload_file(file: UploadFile = File(...)):
+
+    if file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Unsupported file type"
+        )
 
     extension = file.filename.split(".")[-1]
 
@@ -17,7 +30,7 @@ async def upload_file(file: UploadFile = File(...)):
 
     job_id = create_job()
 
-    local_path = f"app/uploads/{filename}"
+    local_path = f"/tmp/{filename}"
 
     content = await file.read()
 
@@ -28,17 +41,11 @@ async def upload_file(file: UploadFile = File(...)):
         local_path,
         f"uploads/{filename}"
     )
-    ALLOWED_TYPES = {
-        "image/jpeg",
-        "image/png",
-        "video/mp4"
-    }
-    if file.content_type not in ALLOWED_TYPES:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Unsupported file type"
-        )
-    elif file.content_type.startswith("image/"):
+
+    if os.path.exists(local_path):
+        os.remove(local_path)
+
+    if file.content_type.startswith("image/"):
         process_image.delay(filename, job_id)
     else:
         process_video.delay(filename, job_id)
--- app/services/image_service.py
+++ app/services/image_service.py
@@ -2,43 +2,58 @@ from PIL import Image
 import ffmpeg
 from ..services.s3_client import upload_to_s3, download_from_s3
 from ..utils.img_utils import crop_center, resize_to_square, add_watermark
+import os
 
 
 def resize_image(filename):
 
     local_input = f"/tmp/{filename}"
-
-    download_from_s3(f"uploads/{filename}", local_input)
-    img = Image.open(local_input)
-
-    img = crop_center(img)
-
-    img = resize_to_square(img)
-
-    img = add_watermark(img)
-
-    img.save(f"app/processed/{filename}")
-
     local_output = f"/tmp/processed_{filename}"
 
-    img.save(local_output, optimize=True, quality=75)
+    download_from_s3(f"uploads/{filename}", local_input)
+    with Image.open(local_input) as img:
+        img = crop_center(img)
+        img = resize_to_square(img)
+        img = add_watermark(img)
+        img.save(local_output, optimize=True, quality=75)
 
-    upload_to_s3(
-        local_output,
-        f"processed/{filename}"
-    )
+    try:
+        upload_to_s3(
+            local_output,
+            f"processed/{filename}"
+        )
+    finally:
+        if os.path.exists(local_input):
+            os.remove(local_input)
+
+        if os.path.exists(local_output):
+            os.remove(local_output)
 
 
 def compress_video(filename):
-    ffmpeg.input(f"app/uploads/{filename}").output(
-        f"app/processed/{filename}",
-        vcodec="libx264"
-    ).run()
+    local_input = f"/tmp/{filename}"
 
-    upload_to_s3(
-        f"app/processed/{filename}",
-        f"processed/{filename}"
+    download_from_s3(
+        f"uploads/{filename}",
+        local_input
     )
+
+    local_output = f"/tmp/processed_{filename}"
+
+    ffmpeg.input(local_input).output(local_output, vcodec="libx264").run(
+        overwrite_output=True, quiet=True)
+
+    try:
+        upload_to_s3(
+            local_output,
+            f"processed/{filename}"
+        )
+    finally:
+        if os.path.exists(local_input):
+            os.remove(local_input)
+
+        if os.path.exists(local_output):
+            os.remove(local_output)
 
 
 def generate_thumbnail(filename):
@@ -55,9 +70,17 @@ def generate_thumbnail(filename):
     )
 
     ffmpeg.input(local_video, ss=1).output(
-        local_thumbnail, vframes=1).overwrite_output().run()
+        local_thumbnail, vframes=1).overwrite_output().run(overwrite_output=True, quiet=True)
 
-    upload_to_s3(
-        local_thumbnail,
-        f"thumbnails/{thumbnail_name}"
-    )
+    try:
+        upload_to_s3(
+            local_thumbnail,
+            f"thumbnails/{thumbnail_name}"
+        )
+
+    finally:
+        if os.path.exists(local_video):
+            os.remove(local_video)
+
+        if os.path.exists(local_thumbnail):
+            os.remove(local_thumbnail)
